ec85fd689f80025314667675aba60829e15ee956
Mbonea-Mjema <mbonegd@yahoo.com>
2018-10-08 22:42:22 +0530

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,6 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { FormsModule }   from '@angular/forms';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { AngularFireAuthModule} from 'angularfire2/auth';
 import { environment } from '../environments/environment';
 
 @NgModule({
@@ -23,9 +20,6 @@ import { environment } from '../environments/environment';
     BrowserModule,
     HttpClientModule,
   FormsModule,
-  AngularFireModule.initializeApp(environment.firebase),
-  AngularFirestoreModule, // imports firebase/firestore, only needed for database features
-  AngularFireAuthModule
   ],
   providers: [DataService ],
   bootstrap: [AppComponent]
